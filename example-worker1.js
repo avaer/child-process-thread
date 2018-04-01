b9ac70010314634f3d8a844683665986b1a8d5ad
Avaer Kazmer <a@modules.io>
2018-04-01 00:39:01 -0400

    Add deasync test to example-worker

--- example-worker1.js
+++ example-worker1.js
@@ -4,6 +4,17 @@ onmessage = m => {
   console.log('got thread message', m.data);
   postMessage(m);
 };
+
+console.log('deasync 1', typeof deasync);
+deasync(cb => {
+  console.log('deasync 2', typeof cb);
+
+  cb();
+
+  console.log('deasync 3');
+});
+console.log('deasync 4');
+
 setTimeout(() => {
   console.log('worker 2 1');
 }, 100);
