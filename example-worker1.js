8fde6b8b7ec562a830345a05e0976edc27433a4f
Avaer Kazmer <a@modules.io>
2018-04-01 22:48:57 -0400

    Remove tick functionality from example

--- example-worker1.js
+++ example-worker1.js
@@ -7,33 +7,14 @@ onmessage = m => {
   postMessage(m);
 };
 
-console.log('tick 1', typeof tick);
-tick();
-console.log('tick 3');
-
-// let done = true;
-let done = false;
 fetch('http://www.google.ca/?gfe_rd=cr&amp;dcr=0&amp;ei=YODAWvytDu6fXuLLrOAI')
   .then(res => res.text())
   .then(s => {
     console.log('got res', s.length);
-    done = true;
   })
   .catch(err => {
     console.warn(err.stack);
   });
-/* process.nextTick(() => {
-  done = true;
-}); */
-/* setTimeout(() => {
-  done = true;
-}, 100); */
-while (!done) {
-  tick();
-  console.log('check tick done', done);
-}
-
-console.log('tick 4');
 
 setTimeout(() => {
   console.log('worker 2 1');
